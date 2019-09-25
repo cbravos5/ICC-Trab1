e4342247f45ff8145ed1d307fc509b588c37292a
fernandodruszcz <55147100+fernandodruszcz@users.noreply.github.com>
2019-09-25 17:32:36 -0300

    Add files via upload

--- SistLinear.c
+++ SistLinear.c
@@ -1,11 +1,17 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX 55
 
 struct matPent
 {
 	double *Dp, *Ds, *Di, *Dsa, *Dia, *X, *B;	
 };
+
+double fX(double x, double y)
+{
+	return 4*M_PI*(sin(2*M_PI*x)*sinh(M_PI*y) + sin(2*M_PI*(M_PI-x))*sinh(M_PI*(M_PI-y)));
+}
 
 void PreencheVetores(int nx, int ny, double hx, double hy, struct matPent *A)
 {
@@ -56,6 +62,12 @@ void PreencheVetores(int nx, int ny, double hx, double hy, struct matPent *A)
 		else
 			A->Dia[i] = 0;
 	}
+	// B e X
+	for(int i = 0; i < max; i++){
+		double div = RAND_MAX/(1000 - 100);
+		A->B[i] = rand()/div;
+		A->X[i] = 0;
+	}
 }
 
 
@@ -95,12 +107,62 @@ void printVet(int nx, int ny, struct matPent *A)
 		printf("%f  ", A->Dia[i]);
 	}
 	printf("\n\n");
+
+		printf("B\n");
+	for(int i = 0; i < (nx-2)*(ny-2); i++){
+		printf("%f ", A->B[i]);
+	}
+	printf("\n\n");
+
+		printf("RESULTADO\n");
+	for(int i = 0; i < (nx-2)*(ny-2); i++){
+		printf("%1.10f ", A->X[i]);
+	}
+	printf("\n\n");
+
+
+}
+
+void gs(struct matPent m, int afast, double eps){
+	double Xold[MAX];
+	double e = eps + 1.0;
+	unsigned int j = 0;
+	for(int i = 0; i < MAX; i++)
+		Xold[i] = 0;
+	while(j < eps/*e > eps*/){
+		m.X[0] = (m.B[0] - m.Ds[0]*m.X[1] - m.Dsa[0]*m.X[afast])/m.Dp[0];
+		for(int i = 1; i < afast; i++){
+			m.X[i] = (m.B[i] - m.Di[i]*Xold[i - 1] - m.Ds[i]*m.X[i + 1] - m.Dsa[i]*m.X[i + afast])/m.Dp[i];
+		}
+		for(int i = afast; i < MAX - afast; i++)
+			m.X[i] = (m.B[i] - m.Dia[i]*Xold[i - afast] - m.Di[i]*Xold[i - 1] - m.Ds[i]*m.X[i + 1] - m.Dsa[i]*m.X[i + afast])/m.Dp[i];
+		for(int i = MAX - afast; i < MAX - 1; i++)
+			m.X[i] = (m.B[i] - m.Dia[i]*Xold[i - afast] - m.Di[i]*Xold[i - 1] - m.Ds[i]*m.X[i + 1])/m.Dp[i];
+		m.X[MAX - 1] = (m.B[MAX - 1] - m.Dia[MAX - 1]*Xold[MAX -1 - afast] - m.Di[MAX - 1]*Xold[MAX - 2])/m.Dp[MAX - 1];
+		for(int i = 0; i < MAX; i++)
+			Xold[i] = m.X[i];
+
+		j++;
+
+		if((j % 2) == 0){
+			printf("iteração %d\n", j);
+			for(int i = 0; i < MAX; i++)
+				printf("%1.15f  ", m.X[i]);
+			printf("\n\n");
+		}
+		/*e = X[0] - Xold[0];
+		for(int i = 1; i < MAX; i++){
+			if(fabs(X[i] - Xold [i]) > e)
+				e = fabs(X[i] - Xold[i]);
+		}*/
+	}
 }
 
 void main(int argc[], char **argv[])
 {
 	double nx, ny, hx, hy;
-	nx = ny = 5.0;
+	nx = 13.0;
+	ny = 7.0;
 	hx = hy = 1.567;
 	struct matPent A;
 	A.Dp = malloc((nx-2)*(ny-2)*sizeof(double));
@@ -108,7 +170,10 @@ void main(int argc[], char **argv[])
 	A.Di = malloc((nx-2)*(ny-2)*sizeof(double));
 	A.Dsa = malloc((nx-2)*(ny-2)*sizeof(double));
 	A.Dia = malloc((nx-2)*(ny-2)*sizeof(double));
+	A.X = malloc((nx-2)*(ny-2)*sizeof(double));
+	A.B = malloc((nx-2)*(ny-2)*sizeof(double));
 	PreencheVetores(nx, ny, hx, hy, &A);
+	gs(A,11,10);
 	printVet(nx, ny, &A);
 }
 
