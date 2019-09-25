2c8a3cebc1b79df0b9404516bad71a385b391cf8
Cbravos <50149406+cbravos5@users.noreply.github.com>
2019-09-25 17:19:04 -0300

    Update fronteira.c

--- fronteira.c
+++ fronteira.c
@@ -4,12 +4,6 @@
 #define PI 3.14159265
 
 double u_fronteira_sul(double x){
-	/*double a,b;
-	a = 2*PI*PI - 2*PI*x;
-	b = PI*PI;
-	a = sin(a);
-	b = sinh(b);
-	return(a*b);*/
 	return(sin((2*M_PI*M_PI) - (2*M_PI*x))*sinh(M_PI*M_PI));
 
 }
@@ -18,35 +12,37 @@ double u_fronteira_norte(double x){
 	return(sin(2*M_PI*x)*sinh(M_PI*M_PI));
 }
 
-x[0] = (b[0] - dsup[0]*x[1])/dprin[0];
-for(int i = 1; i < TAM - 1; i++){
-	x[i] = (b[i] - dinf[i]*x[i - 1] - dsup[i]*x[i + 1])dprin[i];
-}
-x[TAM] = (b[TAM] - dinf[TAM]*x[TAM - 1]);
 
-void gs(struct matPent m, int afast, double eps){
-	double Xold[MAX];
-	double e = eps + 1.0;
-	for(int i = 0; i < MAX; i++)
-		Xold[i] = 0;
-	while(e > eps){
-		m.X[0] = (m.B[0] - m.Ds[0]*m.X[1] - m.Dsa[0]*m.X[afast])/m.Dp[0];
-		for(int i = 1; i < afast - 1; i++){
-			m.X[i] = (m.B[i] - m.Di[i]*m.X[i - 1] - m.Ds[i]*m.X[i + 1] - m.Dsa[i]*m.X[i + afast])/m.Dp[i];
-		}
-		for(int i = afast; i < MAX - afast; i++)
-			m.X[i] = (m.B[i] - m.Dia[i]*m.X[i - afast] - m.Di[i]*m.X[i - 1] - m.Ds[i]*m.X[i + 1] - m.Dsa[i]*m.X[i + afast])/m.Dp[i];
-		for(int i = MAX - afast; i < MAX - 1; i++)
-			m.X[i] = (m.B[i] - m.Dia[i]*m.X[i - afast] - m.Di[i]*m.X[i - 1] - m.Ds[i]*m.X[i + 1])/m.Dp[i];
-		m.X[i] = (m.B[i] - m.Dia[i]*m.X[i - 3] - m.Di[i]*m.X[i - 1])/m.Dp[i];
-		e = X[0] - Xold[0];
-		for(int i = 1; i < MAX; i++){
-			if(fabs(X[i] - Xold [i]) > e)
-				e = fabs(X[i] - Xold[i]);
+double fX(double x, double y)
+{
+	return 4*M_PI*(sin(2*M_PI*x)*sinh(M_PI*y) + sin(2*M_PI*(M_PI-x))*sinh(M_PI*(M_PI-y)));
+}
+
+void contorno(int nx, int ny, struct matPent *A)
+{
+	int max = (nx-2)*(nx-2);
+	int p = -1;
+	double hx2, hy2;
+	hx2 = hx2*hx2;
+	hy2 = hy2*hy2;
+	for (int j = 0; j < max; j++)
+	{
+		for (int i = 0; i < max; ++i)
+		{
+			p = p+1;
+			A->B[p] = 2*hx2*hy2*fX(j*hx,i*hy)
+			//ZEROS DA DIAGONAL INFERIOR
+			if (i == 0 && j > 1)
+				A->Di[p] = 0; 
+			//ZEROS DA DIAGONAL SUPERIOR
+			if (i == nx-2 && j < ny-2)
+				A->Ds[p] = 0;
+			//CONTORNO SUL
+			if(j == 1)
+				A->B[p] += (hy-2)*hx2*u_fronteira_sul(j*hx);
+			//CONTORNO NORTE
+			if(j == ny-2)
+				A->B[p] -= (hy-2)*hx2*u_fronteira_norte(j*hx); 
 		}
 	}
-}
-
-void main(){
-	printf("%lf %lf\n",u_fronteira_sul(0.00324), u_fronteira_norte(0.00324));
 }
